c47d9a5e5c5c6db5b11cff69886a198d0675a9da
Mirco Kraenz <kraenzmirco@gmail.com>
2021-09-05 22:14:07 +0200

    fix(#1): enable github + npm releases

--- .releaserc.js
+++ .releaserc.js
@@ -3,18 +3,18 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    // [
-    //   '@semantic-release/github',
-    //   {
-    //     assets: [
-    //       {
-    //         path: 'build/browser/index.js',
-    //         label: 'tau-js.browser.js',
-    //         name: 'tau-js.browser.js',
-    //       },
-    //     ],
-    //   },
-    // ],
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          {
+            path: 'build/browser/index.js',
+            label: 'tau-js.browser.js',
+            name: 'tau-js.browser.js',
+          },
+        ],
+      },
+    ],
     '@semantic-release/npm',
   ],
 };
