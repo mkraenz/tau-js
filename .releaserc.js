ee460303aba68e28690a13cbb87da87fd809af5e
Mirco Kraenz <kraenzmirco@gmail.com>
2021-09-05 21:17:04 +0200

    fix(#1): npm release - disable github release

--- .releaserc.js
+++ .releaserc.js
@@ -3,21 +3,18 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/github',
-      {
-        assets: [
-          {
-            path: 'build/browser/index.js',
-            label: 'tau-js.browser.js',
-            name: 'tau-js.browser.js',
-          },
-          {
-            path: 'build/main/**/*.{js,ts}',
-            label: 'NodeJS JS+TS distribution',
-          },
-        ],
-      },
-    ],
+    // [
+    //   '@semantic-release/github',
+    //   {
+    //     assets: [
+    //       {
+    //         path: 'build/browser/index.js',
+    //         label: 'tau-js.browser.js',
+    //         name: 'tau-js.browser.js',
+    //       },
+    //     ],
+    //   },
+    // ],
+    '@semantic-release/npm',
   ],
 };
